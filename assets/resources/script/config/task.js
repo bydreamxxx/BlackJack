effaacb9480bfe4e600022ad285f063a8b16950c
lrsxnn <lrs-1@163.com>
2021-11-01 16:46:04 +0800

    更新配置

--- assets/resources/script/config/task.js
+++ assets/resources/script/config/task.js
@@ -2,9 +2,10 @@ var data_task =
 {
     items:
     [
-      { key:1,mainType:2,subType:13,prevId:0,level:0,awardExp:0,awardCoin:1000,awardTimes:1,awardItems:"1001,1000",selectItems:"",target:"",completeLevel:0,limit_time:"",trigger:"",title:"21点",desc:"完成3局21点",b_id:0,language_id:1 },
-      { key:2,mainType:2,subType:13,prevId:0,level:0,awardExp:0,awardCoin:1000,awardTimes:1,awardItems:"1001,1000",selectItems:"",target:"",completeLevel:0,limit_time:"",trigger:"",title:"BLACKJACK",desc:"Complete 3 games of Blackjack",b_id:0,language_id:1 },
-      { key:3,mainType:2,subType:13,prevId:0,level:0,awardExp:0,awardCoin:1000,awardTimes:1,awardItems:"1001,1000",selectItems:"",target:"",completeLevel:0,limit_time:"",trigger:"",title:"BLACKJACK",desc:"पूरा 3 'BLACKJACK'",b_id:0,language_id:1 }
+      { key:1,mainType:2,subType:13,prevId:0,level:0,awardExp:0,awardCoin:1000,awardTimes:1,awardItems:"1001,1000",selectItems:"",target:"",completeLevel:0,limit_time:"",trigger:"",title:"blackjackTitle",desc:"blackjackTask",b_id:0 },
+      { key:2,mainType:2,subType:13,prevId:0,level:0,awardExp:0,awardCoin:1000,awardTimes:1,awardItems:"1001,1000",selectItems:"",target:"",completeLevel:0,limit_time:"",trigger:"",title:"",desc:"",b_id:0 },
+      { key:3,mainType:2,subType:13,prevId:0,level:0,awardExp:0,awardCoin:1000,awardTimes:1,awardItems:"1001,1000",selectItems:"",target:"",completeLevel:0,limit_time:"",trigger:"",title:"",desc:"",b_id:0 },
+      { key:4,mainType:2,subType:13,prevId:0,level:0,awardExp:0,awardCoin:1000,awardTimes:1,awardItems:"1001,1000",selectItems:"",target:"",completeLevel:0,limit_time:"",trigger:"",title:"",desc:"",b_id:0 }
     ],
 
     /**
