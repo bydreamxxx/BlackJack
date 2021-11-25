90bf813cd661fffa5868d725844b4396c562a8d3
lrsxnn <lrs-1@163.com>
2021-11-25 17:58:09 +0800

    日志加密

--- assets/resources/script/blackjack_common/crypto/aes.js
+++ assets/resources/script/blackjack_common/crypto/aes.js
@@ -1,19 +1,34 @@
-var crypto = require('crypto');
-const key = 'sllxibcibinaxuez';
+const key = CryptoJS.enc.Utf8.parse('sllxibcibinaxuez');
 var AES = {
+    // Encrypt(data) {
+    //     const cipher = crypto.createCipheriv('aes-128-cbc', key, key);
+    //     var crypted = cipher.update(data, 'utf8', 'hex');
+    //     crypted += cipher.final('hex');
+    //     return crypted;
+    // },
+    // Decrypt(encrypted) {
+    //     const decipher = crypto.createDecipheriv('aes-128-cbc', key, key);
+    //     var decrypted = decipher.update(encrypted, 'hex', 'utf8');
+    //     decrypted += decipher.final('utf8');
+    //     return decrypted;
+    // }
     Encrypt(data) {
-        const cipher = crypto.createCipheriv('aes-128-cbc', key, key);
-        var crypted = cipher.update(data, 'utf8', 'hex');
-        crypted += cipher.final('hex');
-        return crypted;
-        return data;
+        let srcs = CryptoJS.enc.Utf8.parse(data);
+        let encrypted = CryptoJS.AES.encrypt(srcs, key, {iv: key});
+        return encrypted.ciphertext.toString();
+
+        // const cipher = CryptoJS.algo.AES.createEncryptor(key, {iv: key});
+        // var crypted = cipher.process(data);
+        // crypted += cipher.finalize();
+        // return crypted;
     },
     Decrypt(encrypted) {
-        const decipher = crypto.createDecipheriv('aes-128-cbc', key, key);
-        var decrypted = decipher.update(encrypted, 'hex', 'utf8');
-        decrypted += decipher.final('utf8');
-        return decrypted;
-        return encrypted;
+Z
+
+        // const decipher = CryptoJS.algo.AES.createDecryptor(key, {iv: key});
+        // var decrypted = decipher.process(encrypted);
+        // decrypted += decipher.finalize();
+        // return decrypted;
     }
 };
 module.exports = AES;
